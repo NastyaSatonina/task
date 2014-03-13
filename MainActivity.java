5971e125b322855d843d0bd85c62008b3acfa65b
Satonina Anastasia <satonina.n@gmail.com>
2014-03-13 20:34:45 +0300

    java класс Splash screen

---
+++ MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.myapp;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivity extends ActionBarActivity {
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+     
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+            
+                Intent InHome = new Intent(MainActivity.this,Home_screen.class);
+                MainActivity.this.startActivity(InHome);
+                MainActivity.this.finish();
+            }
+        }, 2000);
+    
+	    
+       if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+ 
+    
+    public static class PlaceholderFragment extends Fragment {
+        public PlaceholderFragment() {
+        }
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
+}
