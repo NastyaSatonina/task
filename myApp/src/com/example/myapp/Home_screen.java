150962f828a4fbc02ad623775e32f359a891c867
Satonina Anastasia <satonina.n@gmail.com>
2014-03-19 22:03:11 +0300

    исправленный проект

--- myApp/src/com/example/myapp/Home_screen.java
+++ myApp/src/com/example/myapp/Home_screen.java
@@ -12,25 +12,23 @@ import android.widget.ListView;
 
 public class Home_screen extends ActionBarActivity {
 	final String LOG_TAG = "myLogs";
-	
+
 	private ListView lv1;
-	private String arr[] =
-	{ "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
-     "11","12","13", "14", "15", "16", "17", "18", "19", "20" };
+	private String arr[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+			"11", "12", "13", "14", "15", "16", "17", "18", "19", "20" };
 	ListView lst;
 	ArrayAdapter<String> adapter;
-	  
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {	   
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.fragment_home_screen);
-		
-		lv1=(ListView)findViewById(R.id.listView1);
-		adapter =  new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1 , arr);
-	    lv1.setAdapter(adapter);
-	  
-    }
-	 	
+
+		lv1 = (ListView) findViewById(R.id.listView1);
+		adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, arr);
+		lv1.setAdapter(adapter);
+
 	}
 
-
+}
